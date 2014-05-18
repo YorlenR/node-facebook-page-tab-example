513ff9386dde23f6a3f50dec64fa4434005ea3b3
Paulo McNally <paulomcnally@gmail.com>
2014-05-18 20:54:22 +0200

    change to post

--- nodemodule.js
+++ nodemodule.js
@@ -16,7 +16,7 @@ app.use( session( config.session ) );
 
 app.use( facebookPageTap( app, config.facebook ) );
 
-app.get( '/', function( req, res ){
+app.post( '/', function( req, res ){
 	res.send( JSON.stringify( req.session, null, 2 ) );
 } );
 
