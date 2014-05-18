ccda9bdc866e4e6f40bb96b9f1b91f7b0eb41224
Paulo McNally <paulomcnally@gmail.com>
2014-05-18 17:41:45 +0200

    added params to JSON format

--- nodemodule.js
+++ nodemodule.js
@@ -17,7 +17,7 @@ app.use( session( config.session ) );
 app.use( facebookPageTap( app, config.facebook ) );
 
 app.get( '/', function( req, res ){
-	res.send( JSON.stringify( req.session ) );
+	res.send( JSON.stringify( req.session, null, 2 ) );
 } );
 
 app.listen(config.port);
