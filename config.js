cf17a38b595faa2fbbc4aeab4050909c705383ef
Paulo McNally <paulomcnally@gmail.com>
2014-05-18 20:52:16 +0200

    update config

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ var Config = function(){
      * @type {{app: {id: string, secret: string, auth: {callback: string}}}}
      */
     self.facebook = {
-        id: '1475477369355601', // facebook application ID
-        secret: '8e0173ff871c989163286ca9f4533802', // facebook application secret
+        id: '664975803575764', // facebook application ID
+        secret: 'b43412d7b4d39dd135f643088778952a', // facebook application secret
         callback: 'https://mcnallydevelopers.com/apps/facebook/1475477369355601/', // facebook callback url
         scope: ['email'], // https://developers.facebook.com/docs/facebook-login/permissions/v2.0
         page: 'mcnallydev' // facebook page name
@@ -25,7 +25,7 @@ var Config = function(){
      * @type {{secret: string, key: string, cookie: {secure: boolean}}}
      */
     self.session = {
-        secret: 'dsfwewKsdf', // custom hash
+        secret: 'password', // custom hash
         key: 'fb_' + self.facebook.id, // session key name
         cookie: {
             secure: true,
