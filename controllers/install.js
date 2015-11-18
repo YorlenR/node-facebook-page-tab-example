78261eeded2f602550277044ca2a32f976632b51
Paulo McNally <paulomcnally@gmail.com>
2015-11-18 16:09:49 -0600

    fix install controller

--- controllers/install.js
+++ controllers/install.js
@@ -1,8 +1,8 @@
 var util = require('util');
 var config = require('../config');
 
-module.exports.install = function(res, req) {
+module.exports.install = function(req, res) {
   var url = 'https://www.facebook.com/dialog/pagetab?app_id=%s&redirect_uri=%s';
   var urlFormat = util.format(url, config.facebook.id, 'http://facebook.com');
-  res.redirect(urlFormat);
+  res.redirect(301, urlFormat);
 }
