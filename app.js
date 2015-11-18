10aeb99c2cb18d2ff94853079df718a69293cf08
Paulo McNally <paulomcnally@gmail.com>
2015-11-18 15:36:17 -0600

    rename file

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var facebookPageTap = require('facebook-page-tab');
+var config = require('./config');
+
+// Set in app express module
+var app = express();
+
+// Express configuration
+
+app.use( bodyParser() );
+app.use( cookieParser( config.session.secret ) );
+app.use( session( config.session ) );
+
+app.use( facebookPageTap( app, config.facebook ) );
+
+app.post( '/', function( req, res ){
+	res.send( JSON.stringify( req.session, null, 2 ) );
+} );
+
+app.listen(config.port);
