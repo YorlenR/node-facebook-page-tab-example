aac4818864aba50e4d2402949841ed6ca185f427
Paulo McNally <paulomcnally@gmail.com>
2015-11-18 15:32:56 -0600

    use env

--- config.js
+++ config.js
@@ -6,16 +6,16 @@ var Config = function(){
      * Http Port
      * @type {number}
      */
-    self.port = 1772;
+    self.port = process.env.PORT || 5000;
 
     /**
      * Facebook Application Settings
      * @type {{app: {id: string, secret: string, auth: {callback: string}}}}
      */
     self.facebook = {
-        id: '664975803575764', // facebook application ID
-        secret: 'b43412d7b4d39dd135f643088778952a', // facebook application secret
-        callback: 'https://mcnallydevelopers.com/apps/facebook/1475477369355601/', // facebook callback url
+        id: process.env.FACEBOOK_ID, // facebook application ID
+        secret: process.env.FACEBOOK_SECRET, // facebook application secret
+        callback: process.env.FACEBOOK_CALLBACK, // facebook callback url
         scope: ['email'] // https://developers.facebook.com/docs/facebook-login/permissions/v2.0
     };
 
@@ -24,7 +24,7 @@ var Config = function(){
      * @type {{secret: string, key: string, cookie: {secure: boolean}}}
      */
     self.session = {
-        secret: 'password', // custom hash
+        secret: process.env.SESSION_SECRET, // custom hash
         key: 'fb_' + self.facebook.id, // session key name
         cookie: {
             secure: true,
